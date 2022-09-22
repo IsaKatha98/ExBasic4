311b8f698d78e7db1406bcbad9b51e05561749d5
ikloerzer <ikloerzer@106-14.info2>
2022-09-22 14:03:03 +0200

    C1

---
+++ ExBasic4/src/exbasic4/ExBasic4.java
@@ -0,0 +1,21 @@
+package exbasic4;
+
+public class ExBasic4 {
+
+	public static void main(String[] args) {
+		
+		//Declaro una variable nota1
+		double nota1= 8.9;
+		
+		//Declaro una variable nota2
+		double nota2=7.6;
+		
+		//Declaro la variable media
+		double media=(nota1+nota2)/2;
+		
+		//Mostramos el resultado
+		System.out.println(media);
+		
+	}
+
+}
